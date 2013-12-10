65b1649e2ac346496b2ba40a7ce497c14cd471a4
Esir Pavel <esirpavel@gmail.com>
2013-12-11 02:44:41 +0400

    After adding spike detection

--- hodgkin-huxley-cuda-neuronet.cpp
+++ hodgkin-huxley-cuda-neuronet.cpp
@@ -19,6 +19,8 @@ float E_K   = -77.0f;
 float E_Na  = 55.0f;
 float E_L   = -54.4f;
 
+float V_peak = 30.0f;
+
 //connection parameters
 float tau_psc = 0.2f;
 float exp_psc = exp(-h/tau_psc);
@@ -39,62 +41,77 @@ float hh_h_ch(float V, float h_ch){
 	return (.07f*(1.0f - h_ch)*exp(-(V + 65.0f)/20.0f) - h_ch/(1.0f + exp(-(V + 35.0f)/10.0f)))*h;
 }
 
+void hod_hux_RK4(int n){
+	V_m = V_ms[n];
+	n_ch = n_chs[n];
+	m_ch = m_chs[n];
+	h_ch = h_chs[n];
+	v1 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
+	n1 = hh_n_ch(V_ms[n], n_chs[n]);
+	m1 = hh_m_ch(V_ms[n], m_chs[n]);
+	h1 = hh_h_ch(V_ms[n], h_chs[n]);
+	V_ms[n] = V_m + v1/2.0f;
+	n_chs[n] = n_ch + n1/2.0f;
+	m_chs[n] = m_ch + m1/2.0f;
+	h_chs[n] = h_ch + h1/2.0f;
+
+	v2 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
+	n2 = hh_n_ch(V_ms[n], n_chs[n]);
+	m2 = hh_m_ch(V_ms[n], m_chs[n]);
+	h2 = hh_h_ch(V_ms[n], h_chs[n]);
+	V_ms[n] = V_m + v2/2.0f;
+	n_chs[n] = n_ch + n2/2.0f;
+	m_chs[n] = m_ch + m2/2.0f;
+	h_chs[n] = h_ch + h2/2.0f;
+
+	v3 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
+	n3 = hh_n_ch(V_ms[n], n_chs[n]);
+	m3 = hh_m_ch(V_ms[n], m_chs[n]);
+	h3 = hh_h_ch(V_ms[n], h_chs[n]);
+	V_ms[n] = V_m + v3;
+	n_chs[n] = n_ch + n3;
+	m_chs[n] = m_ch + m3;
+	h_chs[n] = h_ch + h3;
+
+	v4 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
+	n4 = hh_n_ch(V_ms[n], n_chs[n]);
+	m4 = hh_m_ch(V_ms[n], m_chs[n]);
+	h4 = hh_h_ch(V_ms[n], h_chs[n]);
+	V_ms[n] = V_m + (v1 + 2.0f*v2 + 2.0f*v3 + v4)/6.0f;
+	n_chs[n] = n_ch + (n1 + 2.0f*n2 + 2.0f*n3 + n4)/6.0f;
+	m_chs[n] = m_ch + (m1 + 2.0f*m2 + 2.0f*m3 + m4)/6.0f;
+	h_chs[n] = h_ch + (h1 + 2.0f*h2 + 2.0f*h3 + h4)/6.0f;
+}
+
 int main(){
+	T_sim = SimulationTime/h;
 	malloc_neur_memory();
 	malloc_conn_memory();
 	ini_neurs_from_file();
 	init_conns_from_file();
 	ofstream res_file;
 	res_file.open("res.csv");
-	int T_sim = SimulationTime/h;
-	float V_m, n_ch, m_ch, h_ch;
-	float v1, v2, v3, v4;
-	float n1, n2, n3, n4;
-	float m1, m2, m3, m4;
-	float h1, h2, h3, h4;
 
 	for (int t = 1; t < T_sim; t++){
 		res_file << t*h << "; " << V_ms[0] << "; " << V_ms[1] << "; " << n_chs[0] << "; " << m_chs[0] << "; " << h_chs[0] << "; " << endl;
+
 		for (int n = 0; n < Nneur; n++){
-			V_m = V_ms[n];
-			n_ch = n_chs[n];
-			m_ch = m_chs[n];
-			h_ch = h_chs[n];
-			v1 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
-			n1 = hh_n_ch(V_ms[n], n_chs[n]);
-			m1 = hh_m_ch(V_ms[n], m_chs[n]);
-			h1 = hh_h_ch(V_ms[n], h_chs[n]);
-			V_ms[n] = V_m + v1/2.0f;
-			n_chs[n] = n_ch + n1/2.0f;
-			m_chs[n] = m_ch + m1/2.0f;
-			h_chs[n] = h_ch + h1/2.0f;
+			hod_hux_RK4(n);
+			// checking if there's spike on neuron
+			if (V_ms[n] > V_peak && V_ms_last[n] > V_ms[n] && V_ms_last_[n] < V_ms_last[n]){
+				spike_times[spike_arr_dim*num_spikes[n] + n] = t;
+				num_spikes[n]++;
+				cout << t*h << endl;
+				cout << "V_m: " << V_ms[n] << " V_m_last: " << V_ms_last[n] << " V_last_: " << V_ms_last_[n] << endl;
+			}
+			V_ms_last_[n] = V_ms_last[n];
+			V_ms_last[n] = V_ms[n];
+		}
 
-			v2 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
-			n2 = hh_n_ch(V_ms[n], n_chs[n]);
-			m2 = hh_m_ch(V_ms[n], m_chs[n]);
-			h2 = hh_h_ch(V_ms[n], h_chs[n]);
-			V_ms[n] = V_m + v2/2.0f;
-			n_chs[n] = n_ch + n2/2.0f;
-			m_chs[n] = m_ch + m2/2.0f;
-			h_chs[n] = h_ch + h2/2.0f;
+		for (int s = 0; s < Ncon; s++){
+			I_psns[s]  = (ys[s]*h + I_psns[s])*exp_psc;
+			ys[s] *= exp_psc;
 
-			v3 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
-			n3 = hh_n_ch(V_ms[n], n_chs[n]);
-			m3 = hh_m_ch(V_ms[n], m_chs[n]);
-			h3 = hh_h_ch(V_ms[n], h_chs[n]);
-			V_ms[n] = V_m + v3;
-			n_chs[n] = n_ch + n3;
-			m_chs[n] = m_ch + m3;
-			h_chs[n] = h_ch + h3;
-
-			v4 = hh_Vm(V_ms[n], n_chs[n], m_chs[n], h_chs[n], I_syns[n], I_es[n]);
-			n4 = hh_n_ch(V_ms[n], n_chs[n]);
-			m4 = hh_m_ch(V_ms[n], m_chs[n]);
-			h4 = hh_h_ch(V_ms[n], h_chs[n]);
-			V_ms[n] = V_m + (v1 + 2.0f*v2 + 2.0f*v3 + v4)/6.0f;
-			n_chs[n] = n_ch + (n1 + 2.0f*n2 + 2.0f*n3 + n4)/6.0f;
-			m_chs[n] = m_ch + (m1 + 2.0f*m2 + 2.0f*m3 + m4)/6.0f;
-			h_chs[n] = h_ch + (h1 + 2.0f*h2 + 2.0f*h3 + h4)/6.0f;
 		}
 	}
 	res_file.close();
--- hodgkin-huxley-cuda-neuronet.h
+++ hodgkin-huxley-cuda-neuronet.h
@@ -4,8 +4,11 @@
  *  Created on: 09.12.2013
  *      Author: postdoc3
  */
+#include <cstring>
 
 float* V_ms;
+float* V_ms_last;
+float* V_ms_last_;
 float* m_chs;
 float* n_chs;
 float* h_chs;
@@ -15,6 +18,9 @@ float* I_es;
 float* I_poisson;
 float* y_poisson;
 
+int* spike_times; // spike times for each neuron
+int* num_spikes;  // numver of spikes on eash neuron
+
 float* ys;
 float* I_psns; // partial postsynaptic current on each synapse
 int* pre_conns;
@@ -23,9 +29,19 @@ int* delays; // delays in integration steps
 
 int Nneur = 2;
 int Ncon = 1;
+int T_sim;
+int spike_arr_dim;
+
+float V_m, n_ch, m_ch, h_ch;
+float v1, v2, v3, v4;
+float n1, n2, n3, n4;
+float m1, m2, m3, m4;
+float h1, h2, h3, h4;
 
 void malloc_neur_memory(){
 	V_ms = new float[Nneur];
+	V_ms_last = new float[Nneur];
+	V_ms_last_ = new float[Nneur];
 	m_chs = new float[Nneur];
 	n_chs = new float[Nneur];
 	h_chs = new float[Nneur];
@@ -34,6 +50,13 @@ void malloc_neur_memory(){
 	I_es = new float[Nneur];
 	I_poisson = new float[Nneur];
 	y_poisson = new float[Nneur];
+	// if num-th spike occur at a time t on n-th neuron then,
+	// t is stored in element with index T_sim/5*num + n
+	// spike_times[T_sim/5*num + n] = t
+	spike_arr_dim = Nneur*T_sim/5;
+	spike_times = new int[spike_arr_dim];
+	num_spikes = new int[Nneur];
+	memset(num_spikes, 0, Nneur*sizeof(int));
 }
 
 void malloc_conn_memory(){
@@ -53,8 +76,14 @@ void init_conns_from_file(){
 }
 
 void ini_neurs_from_file(){
-	V_ms[0] = 32.906693f;
-	V_ms[1] = 32.906693f;
+	V_ms[0] = 32.9066f;
+	V_ms[1] = 32.9066f;
+
+	V_ms_last[0] = 32.9067f;
+	V_ms_last[1] = 32.9067f;
+
+	V_ms_last_[0] = 32.906693f;
+	V_ms_last_[1] = 32.906693f;
 
 	m_chs[0] = 0.913177f;
 	n_chs[0] = 0.574678f;
