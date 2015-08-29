66ba867f7de4a1b1fe184f92aa51ce97ce507380
Pavel Esir <esirpavel@gmail.com>
2015-08-29 18:16:32 +0300

    Added separated compilation for gpu and cpu code

--- Makefile
+++ Makefile
@@ -1,18 +1,27 @@
-MPI_INC=-I/common/openmpi/include
-MPI_LIB_PATH=-L/common/openmpi/lib
-MPI_LIB=-lmpi_cxx -lmpi
-# MPI_INC=-I/usr/lib/openmpi/include/openmpi -I/usr/lib/openmpi/include
-# MPI_LIB_PATH=-L/usr/lib -L/usr/lib/openmpi/lib
-# MPI_LIB=-lmpi_cxx -lmpi -ldl -lhwloc
-OBJECT=hodgkin-huxley-cuda-neuronet.cu
+#MPI_INC=-I/common/openmpi/include
+#MPI_LIB_PATH=-L/common/openmpi/lib
+#MPI_LIB=-lmpi_cxx -lmpi
+MPI_INC=-I/usr/lib/openmpi/include/openmpi -I/usr/lib/openmpi/include
+MPI_LIB_PATH=-L/usr/lib -L/usr/lib/openmpi/lib
+MPI_LIB=-lmpi_cxx -lmpi -ldl -lhwloc
+CUDA_INC=-I/usr/local/cuda/include
+CUDA_LIB_PATH=-L/usr/local/cuda/lib64/
+
+OBJECT=hodgkin-huxley-cuda-neuronet.cpp
 
 all: hh-cuda
 
-hh-cuda: $(OBJECT) hodgkin-huxley-cuda-neuronet.h
+hh-cuda: $(OBJECT) hodgkin-huxley-cuda-neuronet.h hh-kernels.h hh-kernels.o
 # 	nvcc -arch sm_21 -Xcompiler -Wall -O3 --use_fast_math $(OBJECT) -o $@
-	nvcc -arch sm_21 -Xcompiler -Wall -O3 --use_fast_math -Xcompiler -pthread -DWITH_MPI $(MPI_INC) $(OBJECT) $(MPI_LIB) $(MPI_LIB_PATH) -o $@
+#	nvcc -arch sm_21 -Xcompiler -Wall -O3 --use_fast_math -Xcompiler -pthread -DWITH_MPI $(MPI_INC) $(OBJECT) $(MPI_LIB) $(MPI_LIB_PATH) -o $@
+	g++ -O3 -pthread -DWITH_MPI $(MPI_INC) $(CUDA_INC) $(CUDA_LIB_PATH) $(MPI_LIB_PATH) hh-kernels.o $(OBJECT) $(MPI_LIB) -lcudart_static -ldl -lpthread -lrt -o $@
+#	g++ -O3 -pthread $(CUDA_INC) $(CUDA_LIB_PATH) hh-kernels.o $(OBJECT) -lcudart_static -ldl -lpthread -lrt -o $@
 
-debug: $(OBJECTS) hodgkin-huxley-cuda-neuronet.h
+hh-kernels.o: hh-kernels.cu
+#	nvcc -c -arch sm_21 -Xcompiler -Wall -O3 --use_fast_math -DOSCILL_SAVE hh_kernels.cu -o $@
+	nvcc -c -arch sm_21 -Xcompiler -Wall -O3 --use_fast_math hh-kernels.cu -o $@
+
+debug: $(OBJECT) hodgkin-huxley-cuda-neuronet.h
 	nvcc -arch sm_21 -Xcompiler -g3 -Xcompiler -Wall -O0 $(OBJECT) -o hh-cuda
 clean:
-	rm hh-cuda
+	rm hh-cuda hh-kernels.o
--- hodgkin-huxley-cuda-neuronet.cpp
+++ hodgkin-huxley-cuda-neuronet.cpp
@@ -37,186 +37,6 @@ char ivp_fname[500] = "";
 
 unsigned int Tstart = 1;
 
-__device__ __host__ float get_random(unsigned int *seed){
-	// return random number homogeneously distributed in interval [0:1]
-	unsigned long a = 16807;
-	unsigned long m = 2147483647;
-	unsigned long x = (unsigned long) *seed;
-	x = (a * x) % m;
-	*seed = (unsigned int) x;
-	return ((float)x)/m;
-}
-
-__device__ float hh_Vm(float V, float n_ch, float m_ch, float h_ch, float I_syn, float I_e, float h){
-	return (I_e - g_K*(V - E_K)*n_ch*n_ch*n_ch*n_ch - g_Na*(V - E_Na)*m_ch*m_ch*m_ch*h_ch - g_L*(V - E_L) + I_syn)*h*Cm;
-}
-
-__device__ float hh_n_ch(float V, float n_ch, float h){
-	float temp = 1.0f - expf(-(V + 55.0f)*0.1f);
-	if (temp != 0.0f){
-		return (.01f*(1.0f - n_ch)*(V + 55.0f)/temp - 0.125*n_ch*expf(-(V + 65.0f)*0.0125f))*h;
-	} else {
-//		printf("Деление на ноль, n! \n");
-//      to understand why it'so, calculate the limit for v/(1 - exp(v/10)) then v tend to 0
-		return (0.1f*(1.0f - n_ch)- 0.125*n_ch*expf(-(V + 65.0f)*0.0125f))*h;
-	}
-}
-
-__device__ float hh_m_ch(float V, float m_ch, float h){
-	float temp = 1.0f - expf(-(V + 40.0f)*0.1f);
-	if (temp != 0.0f){
-		return (0.1f*(1.0f - m_ch)*(V + 40.0f)/temp - 4.0f*m_ch*expf(-(V + 65.0f)*0.055555556f))*h;
-	} else {
-//		printf("Деление на ноль, m! \n");
-		return ((1.0f - m_ch) - 4.0f*m_ch*expf(-(V + 65.0f)*0.055555556f))*h;
-	}
-}
-
-__device__ float hh_h_ch(float V, float h_ch, float h){
-	return (.07f*(1.0f - h_ch)*expf(-(V + 65.0f)*0.05f) - h_ch/(1.0f + expf(-(V + 35.0f)*0.1f)))*h;
-}
-
-__global__ void init_poisson(unsigned int* psn_time, unsigned int *psn_seed, unsigned int seed, float rate, float h, int Nneur, int BundleSize){
-	int n = blockIdx.x*blockDim.x + threadIdx.x;
-	int neur = n % BundleSize;
-	if (n < Nneur){
-		psn_seed[n] = 100000*(seed + neur + 1);
-		psn_time[n] = 1 -(1000.0f/(h*rate))*logf(get_random(psn_seed + n));
-	}
-}
-
-__global__ void init_noise(curandState* state, float* Inoise, float* D, unsigned int seed, int Nneur, int BundleSize){
-	int n = blockIdx.x*blockDim.x + threadIdx.x;
-	int neur = n % BundleSize;
-	if (n < Nneur){
-		curand_init(0, neur + seed, 0, &state[n]);
-//		Inoise[n] += sqrt(D[n]/tau_cor)*curand_normal(&state[n]);
-	}
-}
-
-__global__ void integrate_synapses(float* y, float* weight, int* delay, int* pre_conn, int* post_conn,
-		int* spike_time, unsigned int* num_spike_syn, int* num_spike_neur, int t, int Nneur, int Ncon){
-	int s = blockDim.x*blockIdx.x + threadIdx.x;
-	if (s < Ncon){
-		int pre_neur = pre_conn[s];
-		// if we processed less spikes than there is in presynaptic neuron
-		// we need to check whether new spikes at arrive this moment of time
-		if (num_spike_syn[s] < num_spike_neur[pre_neur]){
-			if (spike_time[Nneur*num_spike_syn[s] + pre_neur] == t - delay[s]){
-				atomicAdd(&y[post_conn[s]], weight[s]);
-				num_spike_syn[s]++;
-			}
-		}
-	}
-}
-
-__global__ void integrate_neurons(
-		float* V_m, float* V_m_last, float* n_ch, float* m_ch, float* h_ch,
-		int* spike_time, int* num_spike_neur,
-		float* I_e, float* y, float* I_syn, float* y_psn, float* I_psn, unsigned int* psn_time, unsigned int* psn_seed,
-		float* exp_w_p, float exp_psc, float rate,
-		int Nneur, int t, float h, float* D, float* Inoise, curandState* state, float* Vrec){
-		int n = blockIdx.x*blockDim.x + threadIdx.x;
-		if (n < Nneur){
-		    float I_syn_last = I_psn[n] + I_syn[n];
-			I_psn[n]  = (y_psn[n]*h + I_psn[n])*exp_psc;
-			y_psn[n] *= exp_psc;
-
-			I_syn[n]  = (y[n]*h + I_syn[n])*exp_psc;
-			y[n] *= exp_psc;
-
-			// if where is poisson impulse on neuron
-			if (psn_time[n] == t){
-				y_psn[n] += exp_w_p[n];
- 				psn_time[n] += 1 + (unsigned int) (-(1000.0f/(rate*h))*logf(get_random(psn_seed + n)));
-//				psn_time[n] += 1 + (unsigned int) (-(1000.0f/(rate*h))*logf(curand_uniform(&state[n])));
-			}
-			float V_mem, n_channel, m_channel, h_channel;
-			float v1, v2, v3, v4;
-			float n1, n2, n3, n4;
-			float m1, m2, m3, m4;
-			float h1, h2, h3, h4;
-			float Inoise_;
-			float ns1, ns2, ns3, ns4;
-
-			float dNoise = 0.0f;
-// 			float dNoise = sqrtf(2.0f*h*D[n])*curand_normal(&state[n]);
-//			Inoise[n] = sqrtf(h*D[n])*curand_normal(&state[n])/h;
-
-			V_mem = V_m[n];
-			n_channel = n_ch[n];
-			m_channel = m_ch[n];
-			h_channel = h_ch[n];
-			Inoise_ = Inoise[n];
-			v1 = hh_Vm(V_m[n], n_ch[n], m_ch[n], h_ch[n], I_syn_last + Inoise[n], I_e[n], h);
-			n1 = hh_n_ch(V_m[n], n_ch[n], h);
-			m1 = hh_m_ch(V_m[n], m_ch[n], h);
-			h1 = hh_h_ch(V_m[n], h_ch[n], h);
-			ns1 = (-Inoise[n]*h + dNoise)/tau_cor;
-			V_m[n] = V_mem + v1/2.0f;
-			n_ch[n] = n_channel + n1/2.0f;
-			m_ch[n] = m_channel + m1/2.0f;
-			h_ch[n] = h_channel + h1/2.0f;
-			Inoise[n] = Inoise_ + ns1/2.0f;
-
-			v2 = hh_Vm(V_m[n], n_ch[n], m_ch[n], h_ch[n], (I_syn[n] + I_psn[n] + I_syn_last)/2.0f + Inoise[n] , I_e[n], h);
-			n2 = hh_n_ch(V_m[n], n_ch[n], h);
-			m2 = hh_m_ch(V_m[n], m_ch[n], h);
-			h2 = hh_h_ch(V_m[n], h_ch[n], h);
-			ns2 = (-Inoise[n]*h + dNoise)/tau_cor;
-			V_m[n] = V_mem + v2/2.0f;
-			n_ch[n] = n_channel + n2/2.0f;
-			m_ch[n] = m_channel + m2/2.0f;
-			h_ch[n] = h_channel + h2/2.0f;
-			Inoise[n] = Inoise_ + ns2/2.0f;
-
-
-			v3 = hh_Vm(V_m[n], n_ch[n], m_ch[n], h_ch[n], (I_syn[n] + I_psn[n] + I_syn_last)/2.0f + Inoise[n], I_e[n], h);
-			n3 = hh_n_ch(V_m[n], n_ch[n], h);
-			m3 = hh_m_ch(V_m[n], m_ch[n], h);
-			h3 = hh_h_ch(V_m[n], h_ch[n], h);
-			ns3 = (-Inoise[n]*h + dNoise)/tau_cor;
-			V_m[n] = V_mem + v3;
-			n_ch[n] = n_channel + n3;
-			m_ch[n] = m_channel + m3;
-			h_ch[n] = h_channel + h3;
-			Inoise[n] = Inoise_ + ns3;
-
-
-			v4 = hh_Vm(V_m[n], n_ch[n], m_ch[n], h_ch[n], I_syn[n] + I_psn[n] + Inoise[n], I_e[n], h);
-			n4 = hh_n_ch(V_m[n], n_ch[n], h);
-			m4 = hh_m_ch(V_m[n], m_ch[n], h);
-			h4 = hh_h_ch(V_m[n], h_ch[n], h);
-			ns4 = (-Inoise[n]*h + dNoise)/tau_cor;
-
-			V_m[n] = V_mem + (v1 + 2.0f*v2 + 2.0f*v3 + v4)/6.0f;
-			n_ch[n] = n_channel + (n1 + 2.0f*n2 + 2.0f*n3 + n4)/6.0f;
-			m_ch[n] = m_channel + (m1 + 2.0f*m2 + 2.0f*m3 + m4)/6.0f;
-			h_ch[n] = h_channel + (h1 + 2.0f*h2 + 2.0f*h3 + h4)/6.0f;
-			Inoise[n] = Inoise_ + (ns1 + 2.0f*(ns2 + ns3) + ns4)/6.0f;
-
-//			V_m[n] = V_mem + v1;
-//			n_ch[n] = n_channel + n1;
-//			m_ch[n] = m_channel + m1;
-//			h_ch[n] = h_channel + h1;
-//			Inoise[n] = Inoise_ + ns1;
-
-			// checking if there's spike on neuron
-			if (V_m[n] > V_peak && V_mem > V_m[n] && V_m_last[n] <= V_mem){
-				if (num_spike_neur[n] == 0 || t - spike_time[Nneur*(num_spike_neur[n] - 1) + n] > 5.0f/h){
-					spike_time[Nneur*num_spike_neur[n] + n] = t;
-					num_spike_neur[n]++;
-				}
-			}
-			V_m_last[n] = V_mem;
-#ifdef OSCILL_SAVE
-			if (t % recInt == 0){
-				Vrec[Nneur*(t % T_sim_partial/recInt) + n] = V_m[n];
-			}
-#endif
-		}
-}
-
 using namespace std;
 
 int world_size, world_rank;
@@ -258,9 +78,9 @@ int main(int argc, char* argv[]){
 	clear_oscill_file();
 #endif
 	if (strcmp(ivp_fname, "") == 0){
-        init_poisson<<<dim3(Nneur/NEUR_BLOCK_SIZE + 1), dim3(NEUR_BLOCK_SIZE)>>>(psn_times_dev, psn_seeds_dev, seed, rate, h, Nneur, W_P_BUND_SZ);
+        init_poisson(dim3(Nneur/NEUR_BLOCK_SIZE + 1), dim3(NEUR_BLOCK_SIZE), psn_times_dev, psn_seeds_dev, seed, rate, h, Nneur, W_P_BUND_SZ);
 	}
-	init_noise<<<dim3(Nneur/NEUR_BLOCK_SIZE + 1), dim3(NEUR_BLOCK_SIZE)>>>(noise_states_dev, Inoise_dev, Ds_dev, seed, Nneur, W_P_BUND_SZ);
+	init_noise(dim3(Nneur/NEUR_BLOCK_SIZE + 1), dim3(NEUR_BLOCK_SIZE), noise_states_dev, Inoise_dev, Ds_dev, seed, Nneur, W_P_BUND_SZ);
 
 	time_t curr_time = time(0);
     char* st = asctime(localtime(&curr_time));
@@ -273,11 +93,11 @@ int main(int argc, char* argv[]){
 			save_oscill(t);
     	}
 #endif
-		integrate_neurons<<<dim3((Nneur + NEUR_BLOCK_SIZE - 1)/NEUR_BLOCK_SIZE), dim3(NEUR_BLOCK_SIZE)>>>(V_ms_dev, V_ms_last_dev, n_chs_dev, m_chs_dev, h_chs_dev, spike_times_dev, num_spikes_neur_dev,
+		integrate_neurons(dim3((Nneur + NEUR_BLOCK_SIZE - 1)/NEUR_BLOCK_SIZE), dim3(NEUR_BLOCK_SIZE), V_ms_dev, V_ms_last_dev, n_chs_dev, m_chs_dev, h_chs_dev, spike_times_dev, num_spikes_neur_dev,
 				I_es_dev, ys_dev, I_syns_dev, y_psns_dev, I_psns_dev, psn_times_dev, psn_seeds_dev, exp_w_p_dev, exp_psc, rate, Nneur, t, h,
 				Ds_dev, Inoise_dev, noise_states_dev, Vrec_dev);
 		cudaDeviceSynchronize();
-		integrate_synapses<<<dim3((Ncon + SYN_BLOCK_SIZE -1)/SYN_BLOCK_SIZE), dim3(SYN_BLOCK_SIZE)>>>(ys_dev, weights_dev, delays_dev, pre_conns_dev, post_conns_dev,
+		integrate_synapses(dim3((Ncon + SYN_BLOCK_SIZE -1)/SYN_BLOCK_SIZE), dim3(SYN_BLOCK_SIZE), ys_dev, weights_dev, delays_dev, pre_conns_dev, post_conns_dev,
 				spike_times_dev, num_spikes_syn_dev, num_spikes_neur_dev, t, Nneur, Ncon);
 		cudaDeviceSynchronize();
     	if ((t % T_sim_partial) == 0){
