187406efd9930423f295463dea9e9941403effc6
Pavel Esir <esirpavel@gmail.com>
2015-09-07 10:50:08 +0300

    Fit durations corrected

--- fitDurHist.py
+++ fitDurHist.py
@@ -10,27 +10,19 @@ from numpy import *
 import scipy.stats
 import scipy.optimize
 
-T = 20.
+T = 20.3
 
 (TimesUp, TimesDown, Periods) = np.load("durat_182.5_1.3_5.26_2.1.npy")
 TimesUp *= T
 TimesDown *= T
-#%%
-maxHst = 5000
-binSz = 40
-
-figure(1)
-hist(TimesUp, bins=maxHst/binSz, range=(T, maxHst), edgecolor='none')
-xlabel("Duration, ms")
-title("TumesUp")
 
 maxHst = 500000
-minHst = 5000
-binSz = 1600
-figure(2)
-hist(TimesUp, bins=(maxHst-minHst)/binSz, range=(minHst, maxHst), edgecolor='none')
-xlabel("Duration, ms")
-title("TumesUp")
+minHst = 100
+binSz = 100
+D = 15000
+
+hst, bins = histogram(TimesUp, bins=(maxHst-minHst)/binSz, range=(minHst, maxHst))
+bins = bins[:-1]
 #%%
 def expFun(x, *args):
     A, scale = args
@@ -40,28 +32,43 @@ def powFun(x, *args):
     A, scale = args
     return A/x**scale
 
-maxHst = 500000
-#minHst = 5000
-#maxHst = 5000
-minHst = 80
-binSz = 80
-
-hst, bins = histogram(TimesUp, bins=(maxHst-minHst)/binSz, range=(minHst, maxHst))
-bins = bins[:-1]
-#%%
-params = [1, 0.01]
-fitted_params,_ = scipy.optimize.curve_fit(powFun, bins, hst, p0=params)
-
-figure(1)
-plot(bins, hst)
-plot(bins, powFun(bins, *fitted_params), 'r')
-#%%
-def Fun(x, *args):
+def mixedFun(x, *args):
     A1, A2, scale1, scale2, scale3, b = args
     a = 1/(1 + exp(-scale3*(x - b)))
-    return (1 - a)*A1/x**scale1 + a*A2*exp(-scale2*(x - b))
+    return (1 - a)*A1/x**scale1 + a*A2*exp(-scale2*x)
+#%%
+# left hand of distribution (power law)
+bins1 = bins[:D/binSz]
+hst1 = hst[:D/binSz]
+params = [1, 0.01]
+fitted_params,_ = scipy.optimize.curve_fit(powFun, bins1, hst1, p0=params)
 
+figure(1)
+plot(bins1, hst1)
+plot(bins1, powFun(bins1, *fitted_params), 'r')
+xlabel("Duration[ms]")
+title("Power law fitting")
+legend(["data", "fitted"])
+#%%
+# right hand of distribution (exponential)
+bins2 = bins[D/binSz:]
+hst2 = hst[D/binSz:]
+params = [1, 0.001]
+fitted_params,_ = scipy.optimize.curve_fit(expFun, bins2, hst2, p0=params)
+
+figure(2)
+plot(bins2, hst2)
+plot(bins2, expFun(bins2, *fitted_params), 'r')
+xlabel("Duration[ms]")
+title("Exponential")
+legend(["data", "fitted"])
+#%%
 params = [3.05709573e+08, 196., 1.76, 2.3e-05, 0.0025, 5000]
-fitted_params,_ = scipy.optimize.curve_fit(Fun, bins, hst, p0=params)
+fitted_params,_ = scipy.optimize.curve_fit(mixedFun, bins, hst, p0=params)
+#%%
+figure(3)
 plot(bins, hst)
-plot(bins, Fun(bins, *fitted_params), 'r')
+plot(bins, mixedFun(bins, *fitted_params), 'r', linewidth=2.)
+xlabel("Duration[ms]")
+title("Mixed fitting")
+legend(["data", "fitted"])
