20349a1a931a8efeae5725758ae1095b6e5bf588
Pavel Esir (lobach) <esirpavel@gmail.com>
2015-11-18 14:15:35 +0300

    Duration estimation updated

--- Hist_dur.py
+++ Hist_dur.py
@@ -6,27 +6,22 @@ Created on Mon Oct  6 00:26:39 2014
 """
 
 from __future__ import print_function, division
-import matplotlib
-import matplotlib.pylab as pl
+#import matplotlib
+#import matplotlib.pylab as pl
 import numpy as np
 import sys
 from transition_analys import analys_trans
 
-maxSr = 120
+maxSr = 100
 srHstBins = 40
-timeBin = 20.3/1000.
-T = 20.3
 
 N = 100
-Ie=5.26
-rate = 182.5
+Ie=5.27
+rate = 185.0
 w_n = 1.3
-w_p = 2.1
+w_p = 2.08
 
-varParam = np.arange(2.0, 2.141, 0.01)
-
-res_path = "\\\\isilon\\esir_p\\"
-#res_path = '\\\\isilon\\esir_p\\N_100_rate_182.5_w_n_1.3_Ie_5.26\\'
+res_path = '/home/esir_p/hh_cuda/long_new/'
 
 if len(sys.argv) > 1:
     res_path = sys.argv[1]
@@ -37,14 +32,23 @@ if len(sys.argv) > 2:
 if len(sys.argv) > 3:
     rate = float(sys.argv[3])
 
-path = res_path + 'N_{}_rate_{}_w_n_{}_Ie_{:.2f}\\'.format(N, rate, w_n, Ie)
+#for w_p in np.arange(1.91, 2.02, 0.01):
+var_range = np.arange(1.90, 2.041, 0.01)
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+w_p = var_range[rank]
+
+path = res_path + 'N_{}_rate_{}_w_n_{}_Ie_{:.2f}_h_0.02/'.format(N, rate, w_n, Ie)
+#path = res_path + 'N_{}_rate_{}_w_n_{}_Ie_{:.2f}/'.format(N, rate, w_n, Ie)
 
 Periods = []
 TimesUp = []
 TimesDown = []
 
-for seed in xrange(0, 900):
-    period, time_down, time_up = analys_trans(path + "seed_{}\\awsr_w_p_{:.3f}.npy".format(seed, w_p),
+for seed in xrange(0, 600):
+    period, time_down, time_up = analys_trans(path + "seed_{}/awsr_w_p_{:.3f}.npy".format(seed, w_p),
 					      maxSr=maxSr, srHstBins=srHstBins)
     Periods.extend(period)
     TimesDown.extend(time_down)
@@ -52,26 +56,5 @@ for seed in xrange(0, 900):
 Periods = np.array(Periods)
 TimesDown = np.array(TimesDown)
 TimesUp = np.array(TimesUp)
-Tup_ = TimesUp[np.nonzero(TimesUp > 10)]
-Tdown_ = TimesDown[np.nonzero(TimesDown > 10)]
-np.save("{}\\durat_{}_{}_{}_{}.npy".format(path, rate, w_n, Ie, w_p), (TimesUp, TimesDown, Periods))
-#%%
-maxHst = 100000
-binSz = 400
-pl.figure()
-pl.hist(Tup_*T, bins=maxHst/binSz, range=(0, maxHst), edgecolor='none')
-pl.xlabel("Duration, ms")
-pl.title("TumesUp")
 
-maxHst = 20000
-binSz = 400
-pl.figure()
-pl.hist(Tdown_*T, bins=maxHst/binSz, range=(0, maxHst), edgecolor='none')
-pl.xlabel("Duration, ms")
-pl.title("TumesDown")
-#%%
-#figure("loglog")
-#hst, x = histogram(TimesUp*T, bins=maxHst/binSz, range=(0, maxHst))
-#loglog(x[:-1], hst)
-#xlabel("Duration, ms")
-#title("TumesUp")
+np.save("{}/durat_{}_{}_{}_{:.3f}.npy".format(path, rate, w_n, Ie, w_p), (TimesUp, TimesDown, Periods))
