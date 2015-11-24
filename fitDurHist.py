ab9b99b3fe4d1847b1abe0fd790051a1afa2d098
Pavel Esir <esirpavel@gmail.com>
2015-11-24 14:31:04 +0300

    Added fitDurHist.py

--- fitDurHist.py
+++ fitDurHist.py
@@ -10,22 +10,44 @@ from numpy import *
 import scipy.stats
 import scipy.optimize
 
-T = 20.3
+T = 20.0
 
-(TimesUp, TimesDown, Periods) = np.load("durat_182.5_1.3_5.26_2.1_.npy")
+(TimesUp, TimesDown, Periods) = np.load("trans/durat_185.0_1.3_5.27_1.980.npy")
 TimesUp = np.array(TimesUp, dtype='float32')
 TimesDown = np.array(TimesDown, dtype='float32')
 
-TimesUp *= T
-TimesDown *= T
+TimesUp *= T/1000
+TimesDown *= T/1000
 
-maxHst = 500000
-minHst = 500
-binSz = 500
-D = 15000
 
-hst, bins = histogram(TimesUp, bins=(maxHst-minHst)/binSz, range=(minHst, maxHst))
+D = 3
+
+maxHst = 1000
+minHst = 0.2
+binSz = 0.2
+
+#maxHst2 = 4000
+#minHst2 = D
+#binSz2 = 10
+#
+#maxHst = 4000
+#minHst = 2
+#binSz = 1.0
+
+#bins1 = arange(minHst1, maxHst1, binSz1)
+#bins2 = arange(minHst2, maxHst2, binSz2)
+#bins = concatenate((bins1, bins2))
+
+hst, bins = histogram(TimesUp, bins=int((maxHst-minHst)/binSz), range=(minHst, maxHst))
+#hst, _ = histogram(TimesUp, bins=bins)
+#hst = array(hst, dtype='float')
+#hst[:len(bins1)] /= binSz1
+#hst[len(bins1):] /= binSz2
+
 bins = bins[:-1]
+#figure()
+#plot(bins, hst)
+
 #%%
 def expFun(x, *args):
     A, scale = args
@@ -40,56 +62,43 @@ def mixedFun(x, *args):
     a = 1/(1 + exp(-scale3*(x - b)))
     return (1 - a)*A1/x**scale1 + a*A2*exp(-scale2*x)
 #%%
-# left hand of distribution (power law)
-bins1 = bins[:D/binSz]
-hst1 = hst[:D/binSz]
-params = [1, 0.01]
+#left hand of distribution (power law)
+bins1 = bins[:int(D/binSz)]
+hst1 = hst[:int(D/binSz)]
+params = [1, 1.5]
 fitted_params,_ = scipy.optimize.curve_fit(powFun, bins1, hst1, p0=params)
+A1, scale1 = fitted_params
 
-figure(1)
+figure()
 plot(bins1, hst1)
 plot(bins1, powFun(bins1, *fitted_params), 'r')
-xlabel("Duration[ms]")
+xlabel("Duration[s]")
 title("Power law fitting")
 legend(["data", "fitted"])
 #%%
 # right hand of distribution (exponential)
-bins2 = bins[D/binSz:]
-hst2 = hst[D/binSz:]
-#params = [10, 0.0001]
-params = [60, 1.2e-05]
+bins2 = bins[int(D/binSz):]
+hst2 = hst[int(D/binSz):]
+params = [60, 2e-03]
 fitted_params,_ = scipy.optimize.curve_fit(expFun, bins2, hst2, p0=params)
+A2, scale2 = fitted_params
 
-figure(2)
+figure()
 plot(bins2, hst2)
 plot(bins2, expFun(bins2, *fitted_params), 'r')
-xlabel("Duration[ms]")
+xlabel("Duration[s]")
 title("Exponential")
 legend(["data", "fitted"])
 #%%
-params = [3.05709573e+08, 1960., 1.76, 2.3e-05, 0.0025, 5000]
+params = [A1, A2, scale1, scale2,  1.887, -0.157]
 fitted_params,_ = scipy.optimize.curve_fit(mixedFun, bins, hst, p0=params)
 A1, A2, scale1, scale2, scale3, b = fitted_params
 #%%
-figure(3)
+figure()
 loglog(bins, hst, '.')
 loglog(bins, mixedFun(bins, *fitted_params), '--r', linewidth=1.5)
-xlabel(r"$Duration\ Times\ Up,\ ms$")
-ylabel(r"$Counts$")
-title("Mixed fitting")
-legend(["data", "fitted"])
-#%%
-hst, bins = histogram(TimesDown, bins=(maxHst-minHst)/binSz, range=(minHst, maxHst))
-bins = bins[:-1]
-
-params = [3.46+04, 60., 0.753, 1.2e-05, 0.25, 1000]
-fitted_params,_ = scipy.optimize.curve_fit(mixedFun, bins, hst, p0=params)
-A1, A2, scale1, scale2, scale3, b = fitted_params
-
-figure(4)
-loglog(bins, hst, '.')
-loglog(bins, mixedFun(bins, *fitted_params), '--r', linewidth=1.5)
-xlabel(r"$Duration\ Times\ Down,\ ms$")
+#loglog(bins, mixedFun(bins, *params), '--r', linewidth=1.5)
+xlabel(r"$Duration\ Times\ Up,\ s$")
 ylabel(r"$Counts$")
 title("Mixed fitting")
 legend(["data", "fitted"])
